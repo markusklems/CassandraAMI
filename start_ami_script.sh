7678c489011873ec1cfa704d4b9eb84528cd2573
markus <markusklems@gmail.com>
2012-10-29 16:02:24 -0700

    Small change.

--- start_ami_script.sh
+++ start_ami_script.sh
@@ -20,4 +20,4 @@ echo 1 | sudo tee /proc/sys/vm/overcommit_memory
 # Clear old ami.log
 echo "\n======================================================\n" >> ami.log
 cd "$HOME/cassandra_ami"
-sudo sh configure.sh
+git pull
