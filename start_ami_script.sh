3e88dc9a6d935c2d4243cc57632df4544220f72e
markus <markusklems@gmail.com>
2012-10-29 00:49:31 -0700

    Start script that executes when the instance is launched.

---
+++ start_ami_script.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+### BEGIN INIT INFO
+# Provides:
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:
+# Short-Description: Start AMI Configurations on boot.
+# Description:       Enables AMI Configurations on startup.
+### END INIT INFO
+
+# Make sure variables get set
+export JAVA_HOME=/opt/java/64/jdk1.6.0_31
+
+# Setup system properties
+sudo su -c 'ulimit -n 32768'
+echo 1 | sudo tee /proc/sys/vm/overcommit_memory
+
+# Clear old ami.log
+echo "\n======================================================\n" >> ami.log
+cd /home/ubuntu/datastax_ami
+sh configure.sh
