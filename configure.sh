4962831a228ee4cbf9f554513cd02aa7d40963be
markus <markusklems@gmail.com>
2012-10-30 00:22:41 -0700

    Small change (/etc/security/limits.conf).

--- configure.sh
+++ configure.sh
@@ -13,10 +13,25 @@ export DEBIAN_FRONTEND=noninteractive
 cd "$HOME/cassandra_ami"
 git pull
 
-echo "* soft nofile 32768" | sudo tee -a /etc/security/limits.conf
-echo "* hard nofile 32768" | sudo tee -a /etc/security/limits.conf
-echo "root soft nofile 32768" | sudo tee -a /etc/security/limits.conf
-echo "root hard nofile 32768" | sudo tee -a /etc/security/limits.conf
+sudo rm /etc/security/limits.conf
+cat >"$HOME/limits.conf" <<END_OF_FILE
+* soft nofile 32768
+* hard nofile 32768
+root soft nofile 32768
+root hard nofile 32768
+* soft memlock unlimited
+* hard memlock unlimited
+root soft memlock unlimited
+root hard memlock unlimited
+* soft as unlimited
+* hard as unlimited
+root soft as unlimited
+root hard as unlimited
+END_OF_FILE
+
+sudo mv "$HOME/limits.conf" /etc/security/limits.conf
+sudo chown root:root /etc/security/limits.conf
+sudo chmod 2277 /etc/security/limits.conf
 
 sudo chown -hR ubuntu:ubuntu "$HOME"
 
