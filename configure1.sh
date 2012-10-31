0f38c8ab01efa4175c0bd103867202550d4b1743
markus <markusklems@gmail.com>
2012-10-31 08:41:27 -0700

    Minor change.

--- configure1.sh
+++ configure1.sh
@@ -6,9 +6,6 @@ export DEBIAN_FRONTEND=noninteractive
 # Retrieve the latest version of the scripts.
 (cd /home/ubuntu/cassandra_ami; git pull)
 sleep 2
-sudo chmod +x /home/ubuntu/cassandra_ami/configure2.sh
-sudo chmod +x /home/ubuntu/cassandra_ami/configure_devices.sh
-sudo chmod +x /home/ubuntu/cassandra_ami/configure_devices_as_RAID0.sh
-sudo chmod +x /home/ubuntu/cassandra_ami/configure_devices_as_logical_volumes.sh
+sudo chmod +x /home/ubuntu/cassandra_ami/configure*
 # Now call the other scripts
 sudo sh /home/ubuntu/cassandra_ami/configure2.sh
