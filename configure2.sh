4ee30f346e2ef18f14f5cd91e4dcc7867b5a97cf
markus <markusklems@gmail.com>
2012-11-12 02:20:13 -0800

    change access rights, so that tomcat user can read the system columnfamily for incrementalbackup

--- configure2.sh
+++ configure2.sh
@@ -52,6 +52,8 @@ sudo sed -i -e "s|org.apache.cassandra.thrift.CassandraDaemon|com.netflix.priam.
 # Tomcat user must be allowed to start cassandra.
 # TODO: fine-tune this:
 echo "tomcat7 ALL = NOPASSWD: ALL" | sudo tee -a /etc/sudoers
+# Add tomcat7 user to cassandra group
+# sudo usermod -g cassandra tomcat7
 
 # Install OpsCenter
 sudo apt-get -y install opscenter-free
@@ -89,13 +91,6 @@ sudo chown -R cassandra:cassandra $C_LIB_DIR
 sudo chown -R cassandra:cassandra $C_LOG_DIR
 sudo chown -R cassandra:cassandra $LV_LIB_DIR
 sudo chown -R cassandra:cassandra $LV_LOG_DIR
-# Make this accessible to tomcat7 user.
-# We could add tomcat like this: useradd -G cassandra tomcat7
-# However, the easiest way: chmod 777
-sudo chmod -R 777 $C_LIB_DIR
-sudo chmod -R 777 $C_LOG_DIR
-sudo chmod -R 777 $LV_LIB_DIR
-sudo chmod -R 777 $LV_LOG_DIR
 
 # Move the priam lib to where it belongs.
 sudo cp /home/ubuntu/cassandra_ami/priam.jar /usr/share/cassandra/lib/.
@@ -115,14 +110,14 @@ sudo cp /home/ubuntu/cassandra_ami/Priam.war /var/lib/tomcat7/webapps/.
 # Start Tomcat/Priam-Cassandra + opscenter
 sudo /home/ubuntu/cassandra_ami/start.sh
 	
-# TODO quick fix
+# Wait for services to start.
+sleep 30
+
+# quick fix, not secure:
 sudo chmod -R 777 $C_LIB_DIR
 sudo chmod -R 777 $C_LOG_DIR
 sudo chmod -R 777 $LV_LIB_DIR
 sudo chmod -R 777 $LV_LOG_DIR
-	
-# Wait for services to start.
-sleep 30
 
 # Create ycsb table. Concurrent execution should be no issue (?)
 sudo /home/ubuntu/cassandra_ami/configure_ycsb.sh
