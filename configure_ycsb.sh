5de3884217e99881ce02ad46cd53b1b0d0e7afd4
markus <markusklems@gmail.com>
2012-11-16 01:30:43 -0800

    Should use replication factor 2 at least.

--- configure_ycsb.sh
+++ configure_ycsb.sh
@@ -9,7 +9,7 @@ privateip=$1
   CREATE_TABLE_STATEMENTS=/home/ubuntu/create_usertable
 
   cat >$CREATE_TABLE_STATEMENTS <<END_OF_FILE
-create keyspace usertable with strategy_options = [{replication_factor:1}] and placement_strategy = 'org.apache.cassandra.locator.SimpleStrategy';
+create keyspace usertable with strategy_options = [{replication_factor:2}] and placement_strategy = 'org.apache.cassandra.locator.SimpleStrategy';
 use usertable;
 create column family data with comparator='AsciiType';
 END_OF_FILE
